bdac750cc0e4f9278d9d6627c5a32b579c300054
James Harrison <james.harrison.za@gmail.com>
2019-04-09 15:18:00 +0200

    Added notes for calculations

--- DotNetCoreRestAPI/Controllers/ValuesController.cs
+++ DotNetCoreRestAPI/Controllers/ValuesController.cs
@@ -17,7 +17,9 @@ namespace DotNetCoreRestAPI.Controllers
 				// if (hasNonMatchingTags) return rejected message
 				// if (missingTotal) return rejected message
 				// if (missingCostCentre) then the field in the output should be defaulted to ‘UNKNOWN’. 
-			// 2: Calculate the GST and total excluding GST. The extracted <total> includes GST
+			// 2: Calculate the GST and total excluding GST. The extracted <total> includes GST. 
+				// GST = total * (15/100)
+				// TotalExcludingGST = total - GST 
 			// 3: Return extracted + calculated info
 
 			return value;
