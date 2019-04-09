4d5ba3e99dc26e717bf7a07a58a4222f62e151d6
James Harrison <james.harrison.za@gmail.com>
2019-04-09 15:48:09 +0200

    Renamed project to API

--- DotNetCoreRestAPI/Startup.cs
+++ DotNetCoreRestAPI/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace DotNetCoreRestAPI
+namespace API
 {
     public class Startup
     {
