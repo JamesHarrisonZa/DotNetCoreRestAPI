16f857f11d5efad746bfaeabe05ac38952d095fb
James Harrison <james.harrison.za@gmail.com>
2019-04-09 15:58:19 +0200

    Added services project

---
+++ API.Services/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace API.Services
+{
+    public class Calculator
+    {
+        public decimal GetGst()
+        {
+            //ToDo
+            return 42m;
+        }
+    }
+}
