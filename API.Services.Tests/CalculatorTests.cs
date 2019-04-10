abfe98b963e7f6b0db10f60eb3e9e83e63fe9fbb
James Harrison <james.harrison.za@gmail.com>
2019-04-10 10:48:49 +0200

    Refactor. Changed variable name to sut

--- API.Services.Tests/CalculatorTests.cs
+++ API.Services.Tests/CalculatorTests.cs
@@ -10,8 +10,8 @@ namespace Tests
 		[TestCase(1024.01, 153.6015)]
 		public void GetGst(decimal totalIncludingGst, decimal expected)
 		{
-			var calculator = CreateSut();
-			var actual = calculator.GetGst(totalIncludingGst);
+			var sut = CreateSut();
+			var actual = sut.GetGst(totalIncludingGst);
 			Assert.AreEqual(expected, actual);
 		}
 
@@ -19,8 +19,8 @@ namespace Tests
 		[TestCase(1024.01, 153.6015, 870.4085)]
 		public void GetTotalExcludingGst(decimal totalIncludingGst, decimal gst, decimal expected)
 		{
-			var calculator = CreateSut();
-			var actual = calculator.GetTotalExcludingGst(totalIncludingGst, gst);
+			var sut = CreateSut();
+			var actual = sut.GetTotalExcludingGst(totalIncludingGst, gst);
 			Assert.AreEqual(expected, actual);
 		}
 
